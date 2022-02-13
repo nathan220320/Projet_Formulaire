04e02b5839d470d6958546c4f6028d5b57c144c0
Nathan220320 <rebours22@gmail.com>
2022-02-13 19:37:00 +0100

    [RegEx]
    Ajout du regex pour l'email

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
-let form = document.querySelector('form')
 
+let email = document.getElementById('mail');
+let small = document.getElementById('small1');
+
+email.onkeydown = function(){
+    const regex = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,8}$/;
+    
+    if (regex.test(email.value)){
+        small.innerHTML = 'Email valide'
+        small.classList.remove('notValide')
+        small.classList.add('valide')
+    }else{
+        small.innerHTML = 'Email non valide'
+        small.classList.remove('valide')
+        small.classList.add('notValide')
+    }
+
+}
