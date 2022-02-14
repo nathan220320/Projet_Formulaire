ee34c385d64090d2fcdbcf42926daab3f02afd09
Nathan220320 <rebours22@gmail.com>
2022-02-14 10:11:10 +0100

    [StyleJs]
    Ajout des valide non valid pour les autre iinput

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 
 let email = document.getElementById('mail');
-let small = document.getElementById('small1');
 let nom = document.getElementById('Nom');
+let prenom = document.getElementById('prenom');
+let msg = document.getElementById('msg')
 let git = document.getElementById('icon-git');
 let linkedin = document.getElementById('icon-linkedin')
 let insta = document.getElementById('icon-insta')
@@ -25,11 +26,35 @@ insta.addEventListener('click', function() {
 
 email.onkeydown = function(){
     const regex = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,8}$/;
+    const regex2 = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
+    const regex3 = /^[a-zA-Z0-9-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
     
     if (regex.test(email.value)){
         email.style.border = '2px solid #16BD13'
+        
     }else{
         email.style.border = '2px solid #F81111'
+        
     }
+
+    if (regex2.test(nom.value)){
+        nom.style.border = '2px solid #16BD13'
+    }else{
+        nom.style.border = '2px solid #F81111'
+    }
+
+    if (regex2.test(prenom.value)){
+        prenom.style.border = '2px solid #16BD13'
+    }else{
+        prenom.style.border = '2px solid #F81111'
+    }
+
+    if (regex3.test(msg.value)){
+        msg.style.border = '2px solid #16BD13'
+    }else{
+        msg.style.border = '2px solid #F81111'
+    } 
+
+
 
 }
