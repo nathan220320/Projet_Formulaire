f284cc713dd23c10ad588d7bfc0dda91d38d0c22
Nathan220320 <rebours22@gmail.com>
2022-02-14 09:42:36 +0100

    [Modif]
    Ajout des lien clicable pour les icon

---
+++ php/script.php
@@ -0,0 +1,6 @@
+<?php
+            echo 'Nom : '.$_POST["Nom"].'<br>';
+            echo 'Prénom : ' .$_POST["Prenom"].'<br>';
+            echo 'Mail : ' .$_POST["Mail"].'<br>';
+            echo 'Message : ' .$_POST["Message"].'<br>';
+        ?>
--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,35 @@
 
 let email = document.getElementById('mail');
 let small = document.getElementById('small1');
+let nom = document.getElementById('Nom');
+let git = document.getElementById('icon-git');
+let linkedin = document.getElementById('icon-linkedin')
+let insta = document.getElementById('icon-insta')
+
+
+git.addEventListener('click', function() {
+    window.open('https://github.com/nathan220320');
+})
+
+linkedin.addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/nathan-rebours/');
+})
+
+insta.addEventListener('click', function() {
+    window.open('https://www.facebook.com/nathan.rebours.22');
+})
+
+
+
+
 
 email.onkeydown = function(){
     const regex = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,8}$/;
     
     if (regex.test(email.value)){
-        small.innerHTML = 'Email valide'
-        small.classList.remove('notValide')
-        small.classList.add('valide')
+        email.style.border = '2px solid #16BD13'
     }else{
-        small.innerHTML = 'Email non valide'
-        small.classList.remove('valide')
-        small.classList.add('notValide')
+        email.style.border = '2px solid #F81111'
     }
 
 }
